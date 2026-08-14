7991796233ba23f50f36f5c93b0bda371c4cd479
Galleto <145520558+GALLETAXO@users.noreply.github.com>
2026-08-14 08:01:16 -0600

    First steps

--- src/Script.js
+++ src/Script.js
@@ -1,1 +1,56 @@
 
+let qr;
+
+
+function generateQR(){
+
+    const text = document.getElementById("textInput").value;
+
+    const container = document.getElementById("qrContainer");
+
+
+    if(text === ""){
+        alert("Escribe algo primero");
+        return;
+    }
+
+
+    container.innerHTML = "";
+
+
+    qr = new QRCode(container, {
+
+        text: text,
+
+        width: 200,
+
+        height: 200
+
+    });
+
+
+    document.getElementById("downloadBtn").disabled = false;
+
+}
+
+
+
+function downloadQR(){
+
+    const img = document.querySelector("#qrContainer img");
+
+
+    if(!img){
+        return;
+    }
+
+
+    const link = document.createElement("a");
+
+    link.href = img.src;
+
+    link.download = "codigo_qr.png";
+
+    link.click();
+
+}
